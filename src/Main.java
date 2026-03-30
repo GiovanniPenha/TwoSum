b927920b77d7baf67a714f9d1216e6924833b925
GiovanniPenha <giovanni.lpenha@gmail.com>
2026-03-30 13:59:00 -0300

    Atualizando...

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] resultado = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+
+        System.out.println(Arrays.toString(resultado));
+    }
 }
